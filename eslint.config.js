23fb7c3746b8b02ae0b8fcfb94ea85389d9cde0b
Luís Felipe <luisf.csdev@gmail.com>
2025-04-15 18:03:10 -0300

    feat: project setup

---
+++ eslint.config.js
@@ -0,0 +1,105 @@
+import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import prettier from 'eslint-plugin-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import unicorn from 'eslint-plugin-unicorn';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettierConfig,
+    ],
+    files: ['**/*.{js,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      import: importPlugin,
+      prettier,
+      unicorn,
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        { prefer: 'type-imports' },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { ignoreRestSiblings: true },
+      ],
+      'import/order': [
+        'error',
+        {
+          alphabetize: {
+            caseInsensitive: true,
+            order: 'asc',
+          },
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index'],
+          ],
+          'newlines-between': 'never',
+          pathGroups: [
+            {
+              group: 'internal',
+              pattern: 'root/**/**',
+              position: 'before',
+            },
+            {
+              group: 'internal',
+              pattern: 'tests/**/**',
+              position: 'before',
+            },
+            {
+              group: 'internal',
+              pattern: '@/**/**',
+              position: 'before',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+        },
+      ],
+      'linebreak-style': ['error', 'unix'],
+      'no-useless-return': 'error',
+      'object-shorthand': ['error', 'always'],
+      'prettier/prettier': [
+        'error',
+        {
+          arrowParens: 'always',
+          printWidth: 80,
+          semi: true,
+          singleQuote: true,
+          tabWidth: 2,
+          trailingComma: 'all',
+        },
+      ],
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.ts', '.vue'],
+        },
+      },
+      tailwindcss: {
+        callees: ['twMerge', 'tv', 'cva', 'cn'],
+      },
+    },
+  },
+  { ignores: ['**/node_modules/', '**/dist/'] },
+);
