5b2c89f1f5ab756f4ed646d07801856e279d4b55
Luís Felipe <luisf.csdev@gmail.com>
2025-04-15 19:16:08 -0300

    feat: add search form

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,3 +1,4 @@
+import { SearchForm } from './components/SearchForm';
 import {
   PriceHighlight,
   TransactionsContainer,
@@ -13,6 +14,7 @@ export function Transactions() {
       <Summary />
 
       <TransactionsContainer>
+        <SearchForm />
         <TransactionsTable>
           <tbody>
             <tr>
