598a891c2583890cb915a8cae3d969004b16e6cb
Luís Felipe <luisf.csdev@gmail.com>
2025-04-15 19:03:37 -0300

    feat: add transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,3 +1,8 @@
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles';
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
 
@@ -6,6 +11,29 @@ export function Transactions() {
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Website development</td>
+              <td>
+                <PriceHighlight variant="income">$ 12,000.00</PriceHighlight>
+              </td>
+              <td>Sales</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHighlight variant="outcome">- $ 12,000.00</PriceHighlight>
+              </td>
+              <td>Food</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
